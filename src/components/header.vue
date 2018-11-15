9b367f656c03fc67ddd9b8f39dd1c1cb005abf0e
maixiaojie <778273234@qq.com>
2018-11-15 17:00:02 +0800

    退出登录

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,12 +2,12 @@
   <header class="wk-header">
   	<img class="logo" src="../../static/images/logo.png" />
   	<div class="user">
-  		<el-dropdown trigger="click">
+  		<el-dropdown trigger="click" @command="action">
 	      <span class="el-dropdown-link">
-	        User<i class="el-icon-arrow-down el-icon--right"></i>
+	        {{username}}<i class="el-icon-arrow-down el-icon--right"></i>
 	      </span>
 	      <el-dropdown-menu slot="dropdown">
-	        <el-dropdown-item>退出</el-dropdown-item>
+	        <el-dropdown-item command="logout">退出</el-dropdown-item>
 	      </el-dropdown-menu>
 	    </el-dropdown>
   	</div>
@@ -16,7 +16,27 @@
 
 <script>
 export default {
-  name: 'WkHeader'
+	name: 'WkHeader',
+	data() {
+		return {
+
+		}
+	},
+	computed: {
+		'username'() {
+			return this.$cookie.get('wk_username') || 'User'
+		}
+	},
+	methods: {
+		action(command) {
+			if(command == 'logout') {
+				this.$cookie.delete('wk_token')
+				this.$cookie.delete('wk_username')
+				this.$router.push({name: 'login'})
+			}
+		}
+	}
+	
 }
 </script>
 
