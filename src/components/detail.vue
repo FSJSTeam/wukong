6a0eb93cba1b62d140c7df275e8db15c1ea61d43
maixiaojie <778273234@qq.com>
2018-11-16 11:13:11 +0800

    详细报告列表页数据调试

--- src/components/detail.vue
+++ src/components/detail.vue
@@ -6,6 +6,7 @@
         <el-form :inline="true" :model="formInline" class="searchForm">
           <el-form-item label="Report Type">
             <el-select v-model="formInline.reportType" placeholder="please selected">
+              <el-option label="All" value="All"></el-option>
               <el-option label="RealBug" value="RealBug"></el-option>
               <el-option label="FalsePositive" value="FalsePositive"></el-option>
               <el-option label="Unknown" value="Unknown"></el-option>
@@ -13,6 +14,7 @@
           </el-form-item>
           <el-form-item label="Marked">
             <el-select v-model="formInline.reportBug" placeholder="please selected">
+              <el-option label="All" value="All"></el-option>
               <el-option label="True" value="True"></el-option>
               <el-option label="False" value="False"></el-option>
             </el-select>
@@ -24,13 +26,12 @@
       </el-col>
       <el-col :span="24">
         <el-table :data="bugData" stripe border>
-          <el-table-column prop="bug_id" label="bug ID"></el-table-column>
+          <el-table-column width="80" prop="bug_id" label="bug ID"></el-table-column>
           <el-table-column prop="file_name" label="File"></el-table-column>
           <el-table-column prop="message" label="Message"></el-table-column>
-          <el-table-column sortable prop="bug_type" label="Bug Type"></el-table-column>
           <el-table-column prop="report_type" label="Report Type"></el-table-column>
           <el-table-column prop="comment" label="Comment"></el-table-column>
-          <el-table-column label="操作" width="130">
+          <el-table-column label="操作" width="90">
             <template slot-scope="scope">
               <el-button size="mini" @click="handleDetail(scope.$index, scope.row, 'bug')">查看</el-button>
             </template>
@@ -41,16 +42,19 @@
         <el-pagination class="mt20"
           background
           layout="prev, pager, next"
-          :total="500">
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :total="pageTotal">
         </el-pagination>
       </el-col>
     </el-row>
     <!-- 文件类型 -->
     <el-row v-show="!bugshow">
-      <el-col :span="20" :offset="2">
+      <el-col :span="24">
         <el-form :inline="true" :model="formInline" class="searchForm">
           <el-form-item label="Report Type">
             <el-select v-model="formInline.reportType" placeholder="please selected">
+              <el-option label="All" value="All"></el-option>
               <el-option label="RealBug" value="RealBug"></el-option>
               <el-option label="FalsePositive" value="FalsePositive"></el-option>
               <el-option label="Unknown" value="Unknown"></el-option>
@@ -58,6 +62,7 @@
           </el-form-item>
           <el-form-item label="Marked">
             <el-select v-model="formInline.reportBug" placeholder="please selected">
+              <el-option label="All" value="All"></el-option>
               <el-option label="True" value="True"></el-option>
               <el-option label="False" value="False"></el-option>
             </el-select>
@@ -67,25 +72,28 @@
           </el-form-item>
         </el-form>
       </el-col>
-      <el-col :span="20" :offset="2">
+      <el-col :span="24">
         <el-table :data="filebugData" stripe border>
-          <el-table-column prop="bug_id" label="bug ID"></el-table-column>
+          <el-table-column width="80" prop="bug_id" label="bug ID"></el-table-column>
           <el-table-column prop="message" label="Message"></el-table-column>
           <el-table-column sortable prop="bug_type" label="Bug Type"></el-table-column>
           <el-table-column prop="report_type" label="Report Type"></el-table-column>
           <el-table-column prop="comment" label="Comment"></el-table-column>
-          <el-table-column label="操作" width="130">
+          <el-table-column label="操作" width="90">
             <template slot-scope="scope">
               <el-button size="mini" @click="handleDetail(scope.$index, scope.row, 'file')">查看</el-button>
             </template>
           </el-table-column>
         </el-table>
       </el-col>
-      <el-col :span="20" :offset="2">
+      <el-col :span="24">
         <el-pagination class="mt20"
           background
           layout="prev, pager, next"
-          :total="500">
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="pageSize"
+          :total="pageTotal">
         </el-pagination>
       </el-col>
     </el-row>
@@ -100,6 +108,9 @@ export default {
     return {
       msg: '详细报告列表页',
       type: '',
+      pageTotal: 0,
+      currentPage: 1,
+      pageSize: 10,
       formInline: {
         reportType: '',
         reportBug: '',
@@ -108,25 +119,8 @@ export default {
         _id: 0,
         bug_type: ''
       },
-      bugData: [
-        {
-          bug_id: '56021',
-          file_name: 'code.vue',
-          message: 'bugffer',
-          bug_type: 'bugtype',
-          report_type: 'report_type',
-          comment: 'comment'
-        }
-      ],
-      filebugData: [
-        {
-          bug_id: '550179',
-          message: 'message',
-          bug_type: 'use-after-free',
-          report_type: 'RealBug',
-          comment: 'comment'
-        }
-      ],
+      bugData: [],
+      filebugData: [],
     }
   },
   computed: {
@@ -141,19 +135,60 @@ export default {
     this.type = this.$route.query.type
     this.run_id = this.$route.query.run_id
     this.bug_num = this.$route.query.bug_num
+    this.pageTotal = parseInt(this.bug_num)
     if(this.type == 'file') {
-      this._id = this.$route.query._id      
+      this._id = this.$route.query._id   
+      this.loadFileData()   
     }else if(this.type == 'bug') {
       this.bug_type = this.$route.query.bugtype
-    }
+      this.loadBugData()
+    }else {}
   },
   methods: {
     handleView() {
 
     },
+    handleCurrentChange(val) {
+      this.currentPage = val
+      if(this.type == 'file') {  
+        this.loadFileData()   
+      }else if(this.type == 'bug') {
+        this.loadBugData()
+      }else {}
+    },
+    loadFileData() {
+      var that = this
+      var data = {
+        run_id: this.run_id,
+        file_id: this._id,
+        bug_num: this.currentPage * this.pageSize,
+        token: that.$cookie.get('wk_token'),
+        cmd: 'detail_file'
+      }
+      that.$ajax.post('/', "msg="+JSON.stringify(data) ).then(res => {
+        if(res.data) {
+          that.filebugData = Array.isArray(res.data)? res.data: [res.data]
+        }
+      })
+    },
+    loadBugData() {
+      var that = this
+      var data = {
+        run_id: this.run_id,
+        bugtype: this.bug_type,
+        bug_num: this.currentPage * this.pageSize,
+        token: that.$cookie.get('wk_token'),
+        cmd: 'detail_bugtype'
+      }
+      that.$ajax.post('/', "msg="+JSON.stringify(data) ).then(res => {
+        if(res.data) {
+          that.bugData = Array.isArray(res.data)? res.data: [res.data]
+        }
+      })
+    },
     handleDetail(index, row, type) {
       console.log(index, row, type)
-      this.$router.push({name: 'code', query: {type}})
+      this.$router.push({name: 'code', query: {type, run_id: this.run_id, bug_id: row.bug_id, bug_num: this.bug_num}})
     },
     onSubmit() {
 
