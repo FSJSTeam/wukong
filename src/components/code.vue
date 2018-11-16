6617e98834741af4d3c3bf4fe27fadb1a6f413e8
maixiaojie <778273234@qq.com>
2018-11-16 12:33:51 +0800

    code页面接口测试，添加codemirror包

--- src/components/code.vue
+++ src/components/code.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="wk-content">
     {{ msg }}
+    <el-row>
+      <el-col :span="8"></el-col>
+      <el-col :span="18"></el-col>
+    </el-row>
   </div>
 </template>
 
@@ -9,7 +13,57 @@ export default {
   name: 'WkCode',
   data () {
     return {
-      msg: '代码错误展示页'
+      msg: '代码错误展示页',
+      filename: '',
+      run_id: 0,
+      bug_id: 0,
+    }
+  },
+  mounted() {
+    this.run_id = this.$route.query.run_id
+    this.bug_id = this.$route.query.bug_id
+    this.loadBugs()
+    // this.loadfile()
+    // this.mark()
+  },
+  methods: {
+    loadBugs() {
+      var that = this
+      var data = {
+        run_id: that.run_id,
+        bug_id: that.bug_id,
+        bug_num: 10,
+        token: that.$cookie.get('wk_token'),
+        cmd: 'report'
+      }
+      that.$ajax.post('/', "msg="+JSON.stringify(data) ).then(res => {
+        console.log(res.data)
+      })
+    },
+    loadfile() {
+      var that = this
+      var data = {
+        cmd: 'request_file',
+        file_name: that.filename,
+        token: that.$cookie.get('wk_token')
+      }
+      that.$ajax.post('/', "msg="+JSON.stringify(data) ).then(res => {
+        console.log(res.data)
+      })
+    },
+    mark() {
+      var that = this
+      var data = {
+        cmd: 'report_markbug',
+        run_id: that.run_id,
+        bug_id: that.bug_id,
+        comment: 'comment test',
+        bugtype: 0,
+        token: that.$cookie.get('wk_token')
+      }
+      that.$ajax.post('/', "msg="+JSON.stringify(data) ).then(res => {
+        console.log(res.data)
+      })
     }
   }
 }
